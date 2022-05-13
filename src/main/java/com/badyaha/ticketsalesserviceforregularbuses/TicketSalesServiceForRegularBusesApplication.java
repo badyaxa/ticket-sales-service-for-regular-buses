c675eae440227f2dafa8d3e3dd04372a78da3bf7
Bohdan Stasiv <badyaxa@gmail.com>
2022-05-13 12:39:09 +0300

    let's begin

---
+++ src/main/java/com/badyaha/ticketsalesserviceforregularbuses/TicketSalesServiceForRegularBusesApplication.java
@@ -0,0 +1,13 @@
+package com.badyaha.ticketsalesserviceforregularbuses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketSalesServiceForRegularBusesApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(TicketSalesServiceForRegularBusesApplication.class, args);
+    }
+}
