9f0085fb6d80ef6e8a1bd18d30d0b2d13f975454
Bohdan Stasiv <badyaxa@gmail.com>
2022-05-13 12:51:07 +0300

    add displayAllBeans method

--- src/main/java/com/badyaha/ticketsalesserviceforregularbuses/TicketSalesServiceForRegularBusesApplication.java
+++ src/main/java/com/badyaha/ticketsalesserviceforregularbuses/TicketSalesServiceForRegularBusesApplication.java
@@ -2,12 +2,25 @@ package com.badyaha.ticketsalesserviceforregularbuses;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class TicketSalesServiceForRegularBusesApplication {
 
+    private static ApplicationContext applicationContext;
+
     public static void main(String[] args) {
 
-        SpringApplication.run(TicketSalesServiceForRegularBusesApplication.class, args);
+        applicationContext = SpringApplication.run(TicketSalesServiceForRegularBusesApplication.class, args);
+        System.out.println("------------------------start---------------------------");
+		displayAllBeans();
+		System.out.println("------------------------end---------------------------");
+	}
+
+    public static void displayAllBeans() {
+        String[] allBeanNames = applicationContext.getBeanDefinitionNames();
+        for (String beanName : allBeanNames) {
+            System.out.println(beanName);
+        }
     }
 }
