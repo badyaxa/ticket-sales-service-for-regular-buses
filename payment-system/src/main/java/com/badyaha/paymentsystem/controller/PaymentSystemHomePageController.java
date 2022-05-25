68083e7e821773616c8988b15a668a63732b98ae
Bohdan Stasiv <badyaxa@gmail.com>
2022-05-25 18:53:26 +0300

    add swagger (openApi)

--- payment-system/src/main/java/com/badyaha/paymentsystem/controller/PaymentSystemHomePageController.java
+++ payment-system/src/main/java/com/badyaha/paymentsystem/controller/PaymentSystemHomePageController.java
@@ -8,12 +8,24 @@ public class PaymentSystemHomePageController {
 
     @GetMapping
     public String welcomeMsg() {
-        return  "<ul>" +
-                "  <li><a href=\"http://127.0.0.1:8080\">Welcome. It is ticket sales service for regular buses</a><br></li>" +
-                "  <li><a href=\"http://127.0.0.1:8088\">Ticket Management System</a><br></li>" +
-                "  <li><a href=\"http://127.0.0.1:8099\"><mark><b>Payment System</b></mark></a></li>" +
+        return "<ul>" +
+                "   <li><a href=\"http://127.0.0.1:8080\">Welcome. It is ticket sales service for regular buses</a></li>" +
+
+                "   <li><a href=\"http://127.0.0.1:8088\">Ticket Management System</a>" +
+                "       <ul>" +
+                "           <li><a href=\"http://127.0.0.1:8088/swagger-ui.html\" target=\"_blank\">swagger-ui.html</a></li>" +
+                "       </ul>" +
+                "   </li>" +
+
+                "   <li><a href=\"http://127.0.0.1:8099\"><mark><b>Payment System</b></mark></a>" +
+                "       <ul>" +
+                "           <li><a href=\"http://127.0.0.1:8099/swagger-ui.html\" target=\"_blank\">swagger-ui.html</a></li>" +
+                "       </ul>" +
+                "   </li>" +
                 "</ul>" +
+
                 "<br>" +
+
                 "<a href=\"https://github.com/badyaxa/ticket-sales-service-for-regular-buses/tree/main/payment-system\" target=\"_blank\">github repo</a>";
     }
 }
--- ticket-management-system/src/main/java/com/badyaha/ticketmanagementsystem/controller/TicketManagementSystemHomePageController.java
+++ ticket-management-system/src/main/java/com/badyaha/ticketmanagementsystem/controller/TicketManagementSystemHomePageController.java
@@ -9,11 +9,23 @@ public class TicketManagementSystemHomePageController {
     @GetMapping
     public String welcomeMsg() {
         return  "<ul>" +
-                "  <li><a href=\"http://127.0.0.1:8080\">Welcome. It is ticket sales service for regular buses</a></li>" +
-                "  <li><a href=\"http://127.0.0.1:8088\"><mark><b>Ticket Management System</b></mark></a></li>" +
-                "  <li><a href=\"http://127.0.0.1:8099\">Payment System</a></li>" +
+                "   <li><a href=\"http://127.0.0.1:8080\">Welcome. It is ticket sales service for regular buses</a></li>" +
+
+                "   <li><a href=\"http://127.0.0.1:8088\"><mark><b>Ticket Management System</b></mark></a>" +
+                "       <ul>" +
+                "           <li><a href=\"http://127.0.0.1:8088/swagger-ui.html\" target=\"_blank\">swagger-ui.html</a></li>" +
+                "       </ul>" +
+                "   </li>" +
+
+                "   <li><a href=\"http://127.0.0.1:8099\">Payment System</a>" +
+                "       <ul>" +
+                "           <li><a href=\"http://127.0.0.1:8099/swagger-ui.html\" target=\"_blank\">swagger-ui.html</a></li>" +
+                "       </ul>" +
+                "   </li>" +
                 "</ul>" +
+
                 "<br>" +
+
                 "<a href=\"https://github.com/badyaxa/ticket-sales-service-for-regular-buses/tree/main/ticket-management-system\" target=\"_blank\">github repo</a>";
     }
 }
